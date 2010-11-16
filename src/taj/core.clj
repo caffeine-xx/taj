753c995c4ea1b7fcd2031823f5e4f27c2e01da8a
Dan Cook <danielkcook@gmail.com>
2010-11-16 13:15:10 +0000

    first commit

---
+++ src/taj/core.clj
@@ -0,0 +1,1 @@
+(ns taj.core)
---
+++ test/taj/test/core.clj
@@ -0,0 +1,6 @@
+(ns taj.test.core
+  (:use [taj.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
