cc13e40086b4de1cd69cc45ee7c3c7d0966fa862
Dan Cook <danielkcook@gmail.com>
2010-11-20 12:06:39 +0000

    broken commit! i know it's bad, but hey

--- src/taj/mbt.clj
+++ src/taj/mbt.clj
@@ -1,18 +1,32 @@
 (ns taj.mbt
+ ^{:author "Daniel Cook",
+   :doc "MBTrading Quotes API"}
   (:use [clojure.java.io :only [as-url reader writer]])
   (:use [clojure.xml     :only [parse]])
   (:use [clojure.string  :only [split trim]])
   (:import java.net.Socket))
 
-;; MBT Interface
-(declare mbt-connect)
-(declare mbt-subscribe)
-(declare mbt-unsubscribe)
-(declare mbt-fundamental)
-(declare mbt-ping)
-(declare mbt-close)
-(declare mbt-open?)
+;; MBT low-level interface
+
+(declare mbt-open)  
+(declare mbt-login) 
+(declare mbt-write)  
 (declare mbt-read)
+(declare mbt-parse)
+(declare mbt-open?)
+(declare mbt-close)
+
+;; Higher-level, lazy seq-based interface using agents
+
+(declare mbt!)          ;; connect to MB trading & login
+(declare disconnect!)   ;; disconnect from MB trading
+(declare subscribe!)    ;; subscribe to a quote stream
+(declare unsubscribe!)  ;; unsubscribe from quote stream
+(declare fundamental?)  ;; request fundamentals info - synchronous
+(declare ping!)         ;; ping the server - synchronous, returns ping time (takes optional timeout)
+(declare quotes)        ;; lazy seq of incoming quotes
+
+;; Utilities
 
 (defn mk-socket [host port] (java.net.Socket. host port))
 (defn mk-writer [sock] (java.io.PrintWriter. (writer sock) true))
@@ -20,19 +34,24 @@
 (defn socket-open? [socket] (when socket (not (.isClosed socket))))
 (defn rekey [h m]
   "Transforms the keys of a hashmap into new set of keys,
-   {:k v} => {(m :k) v}
-   Ignores keys not in m"
+   {:k v} => {(or (m :k) :k) v}
+   Leaves keys not in m intact."
   (let [ks (keys h)]
-    (zipmap (map m ks) (map h ks))))
+    (zipmap (map #(or (m %) %) ks) (map h ks))))
 
-(def mbt-quote-types 
-  {:level1  20000
-   :level2  20001
-   :level12 20002
-   :trades  20003
-   :chains  20004})
+;; MBT fields
 
-(def mbt-quote-fields
+(def mbt-in-type
+  {"G" :login-accept
+   "D" :login-deny
+   "1" :level1
+   "2" :level2
+   "3" :trade
+   "N" :fundamental
+   "4" :option-chain
+   "9" :ping})
+
+(def mbt-in-field
   {"1003"  :symbol
    "2002"  :price
    "2003"  :bid
@@ -40,65 +59,23 @@
    "2014"  :timestamp
    "2015"  :date})
 
-(defn split-msg [s]
-  ^{:doc "Splits a string 
-          A|B=1;C=2 into [A {B 1 C 2}]
-          A         into [A nil]"
-    :test }
-  (if (> (count s) 2)
-    (let [[head body] (map trim (split s #"\|"))
-           data (apply hash-map 
-                  (vec (apply concat 
-                    (map #(split % #"=") (split body #";")))))]
-      [head data])
-    [(trim (first s)) nil]))
+(def mbt-out-type
+  {:login       "L"
+   :subscribe   "S"
+   :unsubscribe "U"
+   :fundamenal  "H"
+   :ping        "9"})
 
-(defn mbt-write [mbt cmd]
-  "Writes a line directly to mbt socket"
-  (.println (:writer mbt) cmd)
-  (println "mbt-write: " cmd)
-  mbt)
+(def mbt-subs-type
+  {:level1       20000
+   :level2       20001
+   :level12      20002
+   :trade        20003
+   :option-chain 20004})
 
-(defn mbt-parse [mbt head data]
-    (let [data     (rekey data mbt-quote-fields)
-          qtype    ({"1" :level1} head)
-          cbkey    [(data :symbol) qtype]
-          callback (get-in mbt [:subscriptions cbkey])]
-      (when callback
-        (callback data))
-      mbt))
+;; Low-level interface 
 
-(def mbt-msg-handlers
-  {"G" (fn [mbt head data] (assoc mbt :status :alive))
-   "D" (fn [mbt head data] (assoc mbt :status :denied))
-   "9" (fn [mbt head data] (println "ping!") mbt)
-   "1" mbt-parse})
-
-(defn mbt-handler [mbt line]
-  "Dispatch a line to a handler"
-  (let [[head data] (split-msg line) 
-        handler     (or (mbt-msg-handlers head) 
-                        (fn [mbt head data] 
-                          (println "unknown :" head ":") 
-                          mbt))]
-    (handler mbt head data)))
-
-(defn mbt-read [mbt]
-  "Synchronously reads a line from the socket, and calls the handler"
-  (when (and (mbt-open? mbt) (:reader mbt))
-    (let [line (.readLine (:reader mbt))]
-      (mbt-handler mbt line))))
-
-(defn mbt-open? [mbt]
-  "Returns true if mbt socket is open"
-  (socket-open? (:socket mbt)))
-
-(defn mbt-login [mbt]
-  "Login to Quotes API"
-  (mbt-write mbt (str "L|" "100=" (:username mbt) ";"
-                           "101=" (:password mbt) "")))
-
-(defn get-mbt-session [user pass]
+(defn- get-mbt-session [user pass]
   "Obtain quote server session information from MBT login server & 
    connect socket"
   (let [url     (str "https://www.mbtrading.com/secure/getquoteserverxml.asp?"
@@ -110,8 +87,8 @@
      :host     (:qs_Server session)}
      :port     5020))
 
-(defn mbt-connect
-  "Connect to MBTrading, return a quote-stream agent"
+(defn mbt-open
+  "Connect to MBTrading, return open session"
   ([user pass] (mbt-connect (get-mbt-session user pass)))
   ([session]
     (let [socket (mk-socket (:host session) (:port session))
@@ -120,6 +97,48 @@
                              :writer (mk-writer socket))]
       mbt)))
 
+(defn mbt-open? [mbt]
+  "Returns true if mbt socket is open"
+  (socket-open? (:socket mbt)))
+
+(defn mbt-login 
+  ([mbt user pass]
+    "Login to Quotes API"
+    (mbt-write mbt (str "L|" "100=" user ";"
+                             "101=" pass "")))
+  ([mbt]
+   "Login to Quotes API using session info"
+   (mbt-login (:username mbt) (:password mbt))))
+
+(defn mbt-read [mbt]
+  "Reads a line from socket"
+  (.readLine (:reader mbt) cmd))
+
+(defn mbt-write [mbt cmd]
+  "Writes a line directly to mbt socket"
+  (.println (:writer mbt) cmd))
+
+(defn mbt-parse [line]
+  ^{:doc "Splits a string 
+          A|B=1;C=2 into [A {B 1 C 2}]
+          A         into [A nil]"
+    :test }
+  (let [head (-> s first trim)
+        body (when (> (count s) 2) 
+                   (-> (split s #"\|") trim second))
+        msg-type (or (mbt-in-type head) head)
+        data (when body  
+               (apply hash-map 
+                 (vec (apply concat 
+                        (map #(split % #"=") (split body #";"))))))]
+    [msg-type data]))
+
+(defn mbt-close [mbt]
+  "Closes an MBTrading connection"
+  (when (and mbt (mbt-open? mbt))
+    (.close (:socket mbt))
+    (assoc mbt :socket nil)))
+
 (defn mbt-ping [mbt] (mbt-write mbt "9"))
 
 (defn mbt-subscribe [mbt symb qtype callback]
@@ -127,13 +146,28 @@
   (-> (mbt-write mbt (str "S|" "1003=" symb ";"
                                "2000=" (mbt-quote-types qtype)))
       (assoc-in [:subscriptions [symb qtype]] callback)))
- 
-(defn mbt-close [mbt]
-  "Closes an MBTrading connection"
-  (when (and mbt (mbt-open? mbt))
-    (.close (:socket mbt))
-    (assoc mbt :socket nil)))
 
-;(def mysock (mbt-connect {:host "127.0.0.1" :port 5020 :username "a" :password "b"}))
-;(mbt-close mysock)
+
+;; High-level interface
+
+
+(declare mbt!)          ;; connect to MB trading & login
+(declare disconnect!)   ;; disconnect from MB trading
+(declare subscribe!)    ;; subscribe to a quote stream
+(declare unsubscribe!)  ;; unsubscribe from quote stream
+(declare fundamental?)  ;; request fundamentals info - synchronous
+(declare ping!)         ;; ping the server - synchronous, returns ping time (takes optional timeout)
+(declare quotes)        ;; lazy seq of incoming quotes
+
+(def *mbt* nil)
+
+(defn mbt! [user pass]
+    (let  [conn (-> (mbt-connect) 
+                    (mbt-login user pass))
+          [reply body] (-> (mbt-read conn)
+                           (mbt-parse))]
+      (when (= reply :login-accept)
+          (def *mbt* (-> conn (assoc :status :alive)
+                               agent))
+          (:status @mbt))))
 
--- test/taj/test/mbt.clj
+++ test/taj/test/mbt.clj
@@ -8,7 +8,6 @@
 
 (def conn (promise))  ; mock qs server
 (def ss   nil) 
-
 (def mbt  (atom nil)) ; session
 
 (def conn-data
@@ -32,7 +31,9 @@
     (f)
     (deliver done? true)
     (swap! mbt mbt-close)
-    (close-server ss)))
+    (close-server ss)
+    (Thread/sleep 0.2) ;dodgy
+    (deref done?)))     ;hack.
 
 (defn sock-read []
   (println "sock-read conn: " (:in @conn))
@@ -52,7 +53,7 @@
   (is (= (split-msg "A") ["A" nil])))
 
 (deftest test-rekey 
-  (let [h {:a 1 :b 2}
+  (let [h {:a  1 :b  2}
         m {:a :A :b :B}
         n (rekey h m)]
     (is (= n {:A 1 :B 2}))))
@@ -60,48 +61,53 @@
 (deftest test-connect 
   (println "test-connect")
   (mk-mbt-conn 
-    #(is (mbt-open? @mbt) "Socket connected")))
+    (fn [] (is (mbt-open? @mbt) "Socket connected"))))
 
 (deftest test-mbt-ping 
   (println "test-mbt-ping")
   (mk-mbt-conn 
-    (fn [] 
-      (swap! mbt mbt-ping)
+    #((swap! mbt mbt-ping)
       (is (= "9" (sock-read))))))
-
 
 (deftest test-login-success
   (println "test-login-success")
   (mk-mbt-conn
-    (fn []
+    (fn [] 
       (swap! mbt mbt-login)
       (is (= "L|100=tu;101=tp" (sock-read)) "Login message")
       (sock-write "G|100=tu") 
       (swap! mbt mbt-read)
-      (is (= :alive (:status @mbt)) "Good Login")
-    )))
+      (is (= :alive (:status @mbt)) "Good Login"))))
 
 (deftest test-login-denied
   (println "test-login-denied")
   (mk-mbt-conn
-    (fn []
+    (fn [] 
       (swap! mbt mbt-login)
       (is (= "L|100=tu;101=tp" (sock-read)) "Login message")
       (sock-write "D|100=tu;103=Failz") 
       (swap! mbt mbt-read)
-      (is (= :denied (:status @mbt)) "Login denied")
-    )))
+      (is (= :denied (:status @mbt)) "Login denied"))))
 
+(deftest test-subscribe
+  (println "test-subscribe")
+  (mk-mbt-conn
+    (fn [] 
+      (let [recv? (promise)]
+        (swap! mbt 
+               mbt-subscribe "C" :level1 
+                            (fn [data] 
+                              (is (= {:symbol "C" :price "1.5"} data) "Quote parsed properly")
+                              (deliver recv? true)))
+        (is (= "S|1003=C;2000=20000" (sock-read)) "Correct subscription message")
+        (sock-write "1|1003=C;2002=1.5")
+        (swap! mbt mbt-read)
+        (is (= @recv? true) "Callback actually got called")))))
 
-;(def done (promise))
-;(mk-server done)
-;(sock-read)
-
-;(test-login-success)
-;;; runners
-;(.isClosed (:server-socket ss))
-;(close-server ss)
-;@qs
-;@mbt
-
-
+(defn test-ns-hook []
+  (test-mbt-ping)
+  (test-connect)
+  (test-login-success)
+  (test-login-denied)
+  (test-subscribe)
+  )
