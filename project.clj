2eff5603e89038a4369e6abfa4a167d0cee33ce5
Dan Cook <danielkcook@gmail.com>
2011-01-24 09:34:26 +0000

    aleph dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [incanter "1.2.3"]
-                 [infer "1.0-SNAPSHOT"]]
+                 [infer "1.0-SNAPSHOT"]
+                 [congomongo "0.1.3-SNAPSHOT"]
+                 [clj-time "0.2.0-SNAPSHOT"]
+                 [joda-time "1.6"]
+                 [aleph "0.1.4.1"]]
   :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
-                     [vimclojure/server "2.2.0"]])
+                     [vimclojure/server "2.2.0"]
+                     [clj-doc "0.6.0-SNAPSHOT"]])
